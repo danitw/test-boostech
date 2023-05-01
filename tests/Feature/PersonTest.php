b4c669821762e66272168ed3b91fb1ae8e4767b8
danitw <danidevpy@gmail.com>
2023-05-01 19:38:33 -0300

    improve tests

--- tests/Feature/PersonTest.php
+++ tests/Feature/PersonTest.php
@@ -10,21 +10,29 @@ use Tests\TestCase;
 
 class PersonTest extends TestCase
 {
-    use RefreshDatabase;
+    use RefreshDatabase, WithFaker;
 
     public function test_create(): void
     {
+        $name = $this->faker->name();
+        $email = $this->faker->safeEmail();
+
         $response = $this->post('/api/person/create', [
-            'name' => 'dani',
-            'email' => 'dani@gmail.com'
+            'name' => $name,
+            'email' => $email
         ]);
 
         $response->assertStatus(201);
         $response->assertJsonStructure(['message', 'person']);
         $response->assertJson(['message' => 'Person Created Successfully', 'person' => [
-            'name' => 'dani',
-            'email' => 'dani@gmail.com'
+            'name' => $name,
+            'email' => $email
         ]]);
+
+        $this->assertDatabaseHas('persons', [
+            'name' => $name,
+            'email' => $email
+        ]);
     }
 
     public function test_read(): void
@@ -50,14 +58,22 @@ class PersonTest extends TestCase
     {
         $person = Person::factory()->create();
 
+        $name = $this->faker->name();
+        $email = $this->faker->safeEmail();
+
         $response = $this->put("/api/person/update/$person->id", [
-            'name' => 'dani',
-            'email' => 'dani@gmail.com'
+            'name' => $name,
+            'email' => $email
         ]);
 
         $response->assertStatus(200);
         $response->assertJsonStructure(['status']);
         $response->assertJson(['status' => 'successfully updated person']);
+
+        $this->assertDatabaseHas('persons', [
+            'name' => $name,
+            'email' => $email
+        ]);
     }
 
     public function test_delete(): void
@@ -68,5 +84,9 @@ class PersonTest extends TestCase
 
         $response->assertStatus(200);
         $response->assertJsonStructure(['status']);
+
+        $this->assertDatabaseMissing('persons', [
+            'id' => $person->id
+        ]);
     }
 }
